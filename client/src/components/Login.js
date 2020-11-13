b013139ea389f797a6bbaa74c07d01b46abcb06b
Erick Gonzalez <ericksinbox@gmail.com>
2020-11-13 11:52:52 -0500

    initial commit

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+
+  const [credentials, setCredentials] = useState({
+    credentials: {
+      username: '',
+      password: ''
+    }
+  });
+
+
+
+
+  const handleChange = e => {
+    setCredentials({
+      credentials: {
+        ...credentials,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <p>Sign In</p>
+      <form >
+        <input
+        type="text"
+        name="username"
+        onChange={handleChange}
+        value={credentials.username}
+        />
+
+        <input
+        type="password"
+        name="password"
+        onChange={handleChange}
+        value={credentials.password}
+        />
+      </form>
     </>
   );
 };
